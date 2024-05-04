e30db657e0c439e20cce089fcde70b6cb07bcf05
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-03 19:41:53 -0700

    update server.js

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen( PORT, () => console.log(`Example app listening on port ${port}!`));
