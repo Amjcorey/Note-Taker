ae5f7d55d6ce6c476896c804059ff408f46e078f
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-05 18:47:32 -0700

    Update server.js with removing commented code due to repetition

--- server.js
+++ server.js
@@ -37,63 +37,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Reads the newly added notes from the request body and then adds them to the db.json file
-// const readThenAppendToJson = (content, file) => {
-//   fs.readFile(file, "utf8", (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       const parsedData = JSON.parse(data);
-//       parsedData.push(content);
-//       writeNewNoteToJson(file, parsedData);
-//     }
-//   });
-// };
-
-// Writes data to db.json -> utilized within the readThenAppendToJson function
-// const writeNewNoteToJson = (destination, content) =>
-//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
-//   );
-
-// Post route -> receives a new note, saves it to request body, adds it to the db.json file, and then returns the new note to the client
-// app.post("/api/notes", (req, res) => {
-//   const { title, text } = req.body;
-//   if (title && text) {
-//     const newNote = {
-//       title: title,
-//       text: text,
-//       id: uniqid(),
-//     };
-
-//     readThenAppendToJson(newNote, "Develop/db/db.json");
-
-//     const response = {
-//       status: "success",
-//       body: newNote,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json("Error in posting new note");
-//   }
-// });
-
-// Delete route -> reads the db.json file, uses the json objects uniqids to match the object to be deleted, removes that object from the db.json file, then re-writes the db.json file
-// app.delete("/api/notes/:id", (req, res) => {
-//   let id = req.params.id;
-//   let parsedData;
-//   fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       parsedData = JSON.parse(data);
-//       const filterData = parsedData.filter((note) => note.id !== id);
-//       writeNewNoteToJson("Develop/db/db.json", filterData);
-//     }
-//   });
-//   res.send(`Deleted note with ${req.params.id}`);
-// });
-
 // App.listen
-app.listen( port, () => console.log(`Example app is listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Example app is listening on port ${port}!`)
+);
